59186b37ebc8b46dd1e9aea5c578d2b63fd963c5
Robby Aprianto <robprian@gmail.com>
2025-04-24 23:14:14 +0700

    first commit

---
+++ pages/index.js
@@ -0,0 +1,221 @@
+import Link from '@/components/Link'
+import { PageSEO } from '@/components/SEO'
+import Tag from '@/components/Tag'
+import siteMetadata from '@/data/siteMetadata'
+import { getAllFilesFrontMatter } from '@/lib/mdx'
+import formatDate from '@/lib/utils/formatDate'
+import { RoughNotation } from 'react-rough-notation'
+import NewsletterForm from '@/components/NewsletterForm'
+import ViewCounter from '@/components/ViewCounter'
+
+const MAX_DISPLAY = 3
+
+export async function getStaticProps() {
+  const posts = await getAllFilesFrontMatter('blog')
+
+  return { props: { posts } }
+}
+
+export default function Home({ posts }) {
+  return (
+    <>
+      <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
+      <div>
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
+          {/* Kolom Pertama - Intro */}
+          <div className="pt-6">
+            <h1 className="pb-6 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
+              Hi, I am{' '}
+              <span className="text-primary-color-500 dark:text-primary-color-dark-500">Parth</span>
+            </h1>
+            <h2 className="prose pt-5 text-lg text-gray-600 dark:text-gray-300">
+              {`Welcome to ${siteMetadata.description}. I am a Data Engineer who is passionate about Data Science and Automation. In my free time, I like developing side projects and learning new technologies.`}
+            </h2>
+            <p className="pt-5 text-lg leading-7 text-slate-600 dark:text-slate-300">
+              This is my place for{' '}
+              <RoughNotation
+                animate="true"
+                type="highlight"
+                show={true}
+                color="#DE1D8D"
+                animationDelay={1000}
+                animationDuration={2500}
+                className="text-slate-200"
+              >
+                thoughts, reflections & everything&nbsp;
+              </RoughNotation>
+              in between. Have a good read!
+            </p>
+            <div className="mt-8 text-slate-600 dark:text-slate-400">
+              <span className="text-sm">Press</span>{' '}
+              <span className="rounded-md bg-gray-300 p-1 text-sm text-gray-900 dark:bg-gray-400">
+                ⌘
+              </span>{' '}
+              <span className="text-sm">+ </span>
+              <span className="rounded-md bg-gray-300 p-1 text-sm text-gray-900 dark:bg-gray-400">
+                K
+              </span>{' '}
+              <span className="text-sm">to start</span>
+            </div>
+          </div>
+
+          {/* Kolom Kedua - Links */}
+          <div className="flex flex-col space-y-4 pt-6">
+            <Link href="/projects" className="group relative block">
+              <div className="animate-tilt absolute -inset-0.5 rounded-lg bg-gradient-to-r from-pink-600 to-purple-600 opacity-50 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>
+              <div className="relative flex items-center divide-x divide-gray-600 rounded-lg bg-white px-7 py-4 leading-none dark:bg-black">
+                <span className="flex items-center space-x-5">
+                  <span className="pr-6 text-gray-900 dark:text-gray-100">What I built</span>
+                </span>
+                <span className="pl-6 text-amber-400 transition duration-200 group-hover:text-gray-900 dark:group-hover:text-gray-100">
+                  Projects&nbsp;&rarr;
+                </span>
+              </div>
+            </Link>
+
+            <Link href="https://parthdesai.vercel.app/" className="group relative block">
+              <div className="animate-tilt absolute -inset-0.5 rounded-lg bg-gradient-to-r from-fuchsia-600 to-emerald-600 opacity-50 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>
+              <div className="relative flex items-center divide-x divide-gray-600 rounded-lg bg-white px-7 py-4 leading-none dark:bg-black">
+                <span className="flex items-center space-x-5">
+                  <span className="pr-6 text-gray-900 dark:text-gray-100">Read my story</span>
+                </span>
+                <span className="pl-6 text-indigo-400 transition duration-200 group-hover:text-gray-900 dark:group-hover:text-gray-100">
+                  Website&nbsp;&rarr;
+                </span>
+              </div>
+            </Link>
+
+            <Link href="https://parthdesai.vercel.app/resume.pdf" className="group relative block">
+              <div className="animate-tilt absolute -inset-0.5 rounded-lg bg-gradient-to-r from-pink-600 to-purple-600 opacity-50 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>
+              <div className="relative flex items-center divide-x divide-gray-600 rounded-lg bg-white px-7 py-4 leading-none dark:bg-black">
+                <span className="flex items-center space-x-5">
+                  <span className="pr-6 text-gray-900 dark:text-gray-100">Hire me!</span>
+                </span>
+                <span className="pl-6 text-primary-400 transition duration-200 group-hover:text-gray-900 dark:group-hover:text-gray-100">
+                  Resume&nbsp;&rarr;
+                </span>
+              </div>
+            </Link>
+          </div>
+
+          {/* Kolom Ketiga - Latest Posts */}
+          <div className="pt-6">
+            <h2 className="mb-4 text-2xl font-bold text-gray-900 dark:text-gray-100">Latest</h2>
+            <div className="flex flex-col space-y-4">
+              {posts.slice(0, 3).map((frontMatter) => {
+                const { slug, date, title, summary, tags } = frontMatter
+                return (
+                  <article key={slug} className="space-y-2">
+                    <div className="space-y-1">
+                      <dl>
+                        <dt className="sr-only">Published on</dt>
+                        <dd className="text-sm font-medium leading-6 text-gray-500 dark:text-gray-400">
+                          <time dateTime={date}>Published on {formatDate(date)}</time> •{' '}
+                          <ViewCounter slug={slug} /> views
+                        </dd>
+                      </dl>
+                      <div>
+                        <h3 className="text-xl font-bold leading-8 tracking-tight">
+                          <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
+                            {title}
+                          </Link>
+                        </h3>
+                      </div>
+                      <div className="flex flex-wrap">
+                        {tags.map((tag) => (
+                          <Tag key={tag} text={tag} />
+                        ))}
+                      </div>
+                    </div>
+                    <div className="prose max-w-none text-gray-500 dark:text-gray-400">
+                      {summary}
+                    </div>
+                  </article>
+                )
+              })}
+            </div>
+            <div className="mt-6 flex justify-end text-base font-medium leading-6">
+              <Link
+                href="/blog"
+                className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                aria-label="all posts"
+              >
+                All Posts &rarr;
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+        <h2 className="flex pb-6 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl md:text-5xl">
+          Latest
+        </h2>
+        <hr className="border-gray-200 dark:border-gray-700" />
+        <ul>
+          {!posts.length && 'No posts found.'}
+          {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
+            const { slug, date, title, summary, tags } = frontMatter
+            return (
+              <Link
+                href={`/blog/${slug}`}
+                key={slug}
+                className="group flex bg-transparent bg-opacity-20 px-2 transition duration-100 hover:scale-105 hover:rounded-xl hover:bg-gray-100 dark:hover:bg-gray-800"
+              >
+                <li className="py-6">
+                  <article>
+                    <div className="space-y-2 bg-transparent bg-opacity-20 p-2 transition duration-200 hover:rounded-xl hover:bg-gray-100 dark:hover:bg-gray-800 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+                      <dl>
+                        <dt className="sr-only">Published on</dt>
+                        <dd className="text-sm font-normal leading-6 text-gray-500 dark:text-gray-400">
+                          <time dateTime={date}>{formatDate(date)}</time>
+                          {' • '}
+                          <ViewCounter className="mx-1" slug={slug} />
+                          views
+                        </dd>
+                      </dl>
+                      <div className="space-y-5 xl:col-span-4">
+                        <div className="space-y-1">
+                          <div>
+                            <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                              <Link
+                                href={`/blog/${slug}`}
+                                className="text-gray-900 transition duration-500 ease-in-out hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-500"
+                              >
+                                {title}
+                              </Link>
+                            </h2>
+                          </div>
+                          <div className="flex flex-wrap">
+                            {tags.map((tag) => (
+                              <Tag key={tag} text={tag} />
+                            ))}
+                          </div>
+                          <div className="prose max-w-none pt-5 text-gray-500 dark:text-gray-400">
+                            {summary}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </article>
+                </li>
+              </Link>
+            )
+          })}
+        </ul>
+      </div>
+      {posts.length > MAX_DISPLAY && (
+        <div className="flex justify-end pt-5 text-lg font-normal leading-6">
+          <Link
+            href="/blog"
+            className="special-underline-new text-primary-500 hover:text-gray-100 hover:no-underline dark:text-primary-500 hover:dark:text-gray-100"
+            aria-label="all posts"
+          >
+            All Posts &rarr;
+          </Link>
+        </div>
+      )}
+      {siteMetadata.newsletter.provider !== '' && (
+        <div className="flex items-center justify-center pt-4">{/* <NewsletterForm /> */}</div>
+      )}
+    </>
+  )
+}
